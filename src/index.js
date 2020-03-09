c6f52e83a699ce0045350d98a53e9fb398729a00
DmitryIP <dmitryip@tut.by>
2020-03-10 01:20:19 +0300

    add: function for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strConfig = bracketsConfig.map(item => item.join(''));
+    for (let i = 0; i < strConfig.length; i++) {
+        if (str.indexOf(strConfig[i]) != -1) {
+            str = str.replace(strConfig[i], '');
+            i = -1;
+        };
+    }
+    if (str.length > 0)
+        return false;
+    else
+        return true;
 }
